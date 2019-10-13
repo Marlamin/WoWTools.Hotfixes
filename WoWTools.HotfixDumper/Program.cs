b90b4251e3af93823f102da0191be534ff67f512
Martin Benjamins <marlamin@marlamin.com>
2019-10-13 23:33:03 +0200

    Push HotfixDumper code

---
+++ WoWTools.HotfixDumper/Program.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WoWTools.HotfixDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException("Need DBCache.bin location and DBef dir location!");
+            }
+
+            var hotfixFile = args[0];
+
+            if (!File.Exists(hotfixFile))
+            {
+                throw new FileNotFoundException("File " + hotfixFile + " not found!");
+            }
+
+            var definitionDir = args[1];
+
+            if (!Directory.Exists(definitionDir))
+            {
+                throw new FileNotFoundException("DBD definition directory " + definitionDir + " not found!");
+            }
+
+            var tableHashes = new Dictionary<uint, string>();
+
+            foreach (var file in Directory.GetFiles(definitionDir))
+            {
+                var dbName = Path.GetFileNameWithoutExtension(file);
+                tableHashes.Add(Utils.Hash(dbName.ToUpper()), dbName);
+            }
+
+            var xfthMagic = 'X' << 0 | 'F' << 8 | 'T' << 16 | 'H' << 24;
+
+            uint build;
+
+            var hotfixes = new List<DBCacheEntry>();
+
+            using (var ms = new MemoryStream(File.ReadAllBytes(hotfixFile)))
+            using (var bin = new BinaryReader(ms))
+            {
+                if (bin.ReadUInt32() != xfthMagic)
+                    throw new Exception("Invalid hotfix file!");
+
+                var version = bin.ReadUInt32();
+                if (version != 7)
+                    throw new Exception("Unsupported version: " + version);
+
+                build = bin.ReadUInt32();
+
+                var hash = bin.ReadBytes(32);
+
+                while (bin.BaseStream.Length > bin.BaseStream.Position)
+                {
+                    var hotfix = new DBCacheEntry();
+                    hotfix.header = bin.Read<DBCacheEntryHeader>();
+                    if (tableHashes.ContainsKey(hotfix.header.tableHash))
+                    {
+                        hotfix.tableName = tableHashes[hotfix.header.tableHash];
+                    }
+                    else
+                    {
+                        hotfix.tableName = "UNKNOWN";
+                    }
+
+                    bin.ReadBytes(hotfix.header.dataSize);
+
+                    if (hotfix.header.magic != xfthMagic)
+                        throw new Exception("Invalid hotfix entry magic!");
+
+                    hotfixes.Add(hotfix);
+                }
+            }
+
+            var filteredList = new List<HotfixEntry>();
+            foreach(var hotfix in hotfixes)
+            {
+                if (hotfix.header.pushID == -1)
+                    continue;
+
+                filteredList.Add(new HotfixEntry
+                {
+                    pushID = hotfix.header.pushID,
+                    recordID = hotfix.header.recordID,
+                    isValid = hotfix.header.isValid,
+                    tableName = hotfix.tableName
+                });
+            }
+
+            var cache = new DBCache();
+            cache.build = build;
+            cache.entries = filteredList.ToArray();
+
+            Console.WriteLine(JsonConvert.SerializeObject(cache, Formatting.None));
+        }
+
+        private struct DBCache
+        {
+            public uint build;
+            public HotfixEntry[] entries;
+        }
+        private struct HotfixEntry
+        {
+            public int pushID;
+            public uint recordID;
+            public byte isValid;
+            public string tableName;
+        }
+
+        private struct DBCacheEntryHeader
+        {
+            public uint magic;
+            public int pushID;
+            public uint tableHash;
+            public uint recordID;
+            public int dataSize;
+            public byte isValid;
+            public byte pad0;
+            public byte pad1;
+            public byte pad2;
+        }
+
+        private struct DBCacheEntry
+        {
+            public DBCacheEntryHeader header;
+            public string tableName;
+        }
+    }
+}
---
+++ WoWTools.HotfixDumper/Utils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WoWTools.HotfixDumper
+{
+    class Utils
+    {
+        private static readonly uint[] s_hashtable = new uint[] {
+        0x486E26EE, 0xDCAA16B3, 0xE1918EEF, 0x202DAFDB,
+        0x341C7DC7, 0x1C365303, 0x40EF2D37, 0x65FD5E49,
+        0xD6057177, 0x904ECE93, 0x1C38024F, 0x98FD323B,
+        0xE3061AE7, 0xA39B0FA1, 0x9797F25F, 0xE4444563,
+        };
+
+        public static uint Hash(string s)
+        {
+            uint v = 0x7fed7fed;
+            var x = 0xeeeeeeee;
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = (byte)s[i];
+                v += x;
+                v ^= s_hashtable[(c >> 4) & 0xf] - s_hashtable[c & 0xf];
+                x = x * 33 + v + c + 3;
+            }
+            return v;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static T Read<T>(this BinaryReader bin)
+        {
+            var bytes = bin.ReadBytes(Marshal.SizeOf(typeof(T)));
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            T ret = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            handle.Free();
+            return ret;
+        }
+
+        /// <summary> Reads the NULL terminated string from 
+        /// the current stream and advances the current position of the stream by string length + 1.
+        /// <seealso cref="BinaryReader.ReadString"/>
+        /// </summary>
+        public static string ReadCString(this BinaryReader reader)
+        {
+            return reader.ReadCString(Encoding.UTF8);
+        }
+
+        /// <summary> Reads the NULL terminated string from 
+        /// the current stream and advances the current position of the stream by string length + 1.
+        /// <seealso cref="BinaryReader.ReadString"/>
+        /// </summary>
+        public static string ReadCString(this BinaryReader reader, Encoding encoding)
+        {
+            var bytes = new List<byte>();
+            byte b;
+            while ((b = reader.ReadByte()) != 0)
+                bytes.Add(b);
+            return encoding.GetString(bytes.ToArray());
+        }
+    }
+
+}
