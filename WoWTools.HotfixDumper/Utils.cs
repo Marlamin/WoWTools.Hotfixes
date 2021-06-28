955f4d7cf7cece3eae5a1c5e4d1ea7f3da14c49a
TOM_RUS <tomrus88@gmail.com>
2021-06-28 06:37:02 +0300

    Fixes

--- WoWTools.HotfixDumper/Utils.cs
+++ WoWTools.HotfixDumper/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -34,11 +35,12 @@ namespace WoWTools.HotfixDumper
     {
         public static T Read<T>(this BinaryReader bin)
         {
-            var bytes = bin.ReadBytes(Marshal.SizeOf(typeof(T)));
-            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            T ret = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
-            handle.Free();
-            return ret;
+            Span<byte> bytes = bin.ReadBytes(Unsafe.SizeOf<T>());
+            //var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            //T ret = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            //handle.Free();
+            //return ret;
+            return Unsafe.As<byte, T>(ref bytes[0]);
         }
 
         /// <summary> Reads the NULL terminated string from 
@@ -64,4 +66,97 @@ namespace WoWTools.HotfixDumper
         }
     }
 
+    public static class ByteArrayToHexStringExtensions
+    {
+        private static readonly uint[] _lookup32 = CreateLookup32();
+
+        private static uint[] CreateLookup32()
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("x2");
+                result[i] = s[0] + ((uint)s[1] << 16);
+            }
+            return result;
+        }
+
+        public static string ToHexString(this byte[] bytes)
+        {
+            var lookup32 = _lookup32;
+            Span<char> result = stackalloc char[bytes.Length * 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                var val = lookup32[bytes[i]];
+                result[2 * i] = (char)val;
+                result[2 * i + 1] = (char)(val >> 16);
+            }
+            return new string(result);
+        }
+
+        public static string ToHexString(this Span<byte> bytes)
+        {
+            var lookup32 = _lookup32;
+            Span<char> result = stackalloc char[bytes.Length * 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                var val = lookup32[bytes[i]];
+                result[2 * i] = (char)val;
+                result[2 * i + 1] = (char)(val >> 16);
+            }
+            return new string(result);
+        }
+    }
+
+    public static unsafe class ByteArrayToHexStringExtensionsUnsafe
+    {
+        private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+        private static readonly uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe, GCHandleType.Pinned).AddrOfPinnedObject();
+
+        private static uint[] CreateLookup32Unsafe()
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("x2");
+                if (BitConverter.IsLittleEndian)
+                    result[i] = s[0] + ((uint)s[1] << 16);
+                else
+                    result[i] = s[1] + ((uint)s[0] << 16);
+            }
+            return result;
+        }
+
+        public static string ToHexStringUnsafe(this byte[] bytes)
+        {
+            var lookupP = _lookup32UnsafeP;
+            var result = new string((char)0, bytes.Length * 2);
+            fixed (byte* bytesP = bytes)
+            fixed (char* resultP = result)
+            {
+                uint* resultP2 = (uint*)resultP;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+            return result;
+        }
+
+        public static string ToHexStringUnsafe(this Span<byte> bytes)
+        {
+            var lookupP = _lookup32UnsafeP;
+            var result = new string((char)0, bytes.Length * 2);
+            fixed (byte* bytesP = bytes)
+            fixed (char* resultP = result)
+            {
+                uint* resultP2 = (uint*)resultP;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+            return result;
+        }
+    }
 }
